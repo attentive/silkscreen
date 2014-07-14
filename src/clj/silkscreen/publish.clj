e90134b045d27056b1666f94312f8bc3144a9b80
Tom Lynch <tflynch@gmail.com>
2014-07-15 01:40:28 +0800

    port publishing shell commands to conch

--- src/clj/silkscreen/publish.clj
+++ src/clj/silkscreen/publish.clj
@@ -1,19 +1,17 @@
 (ns silkscreen.publish
-  (:require [net.cgrand.enlive-html :refer [content html-content append]]
-            [me.raynes.fs :refer [mkdirs hidden? file delete list-dir with-cwd]]
+  (:require [net.cgrand.enlive-html :refer [append clone-for content html-content]]
+            [me.raynes.conch :refer [with-programs]]
             [autoclave.core :refer [markdown-to-html markdown-processor]]
             [hiccup.core :refer [html]]
             [clojure.string :as string])
   (:use [clojure.java.shell :only [sh]]
         org.satta.glob
+        [silkscreen.pygmentize :only [pygmentize-node]]
         [silkscreen.path :only [ensure-path]]
         [silkscreen.post :only [read-file]]
         [silkscreen.conduit :only [defconduit]]))
 
-(defonce md (markdown-processor :fenced-code-blocks))
-
-(defmacro print-sh [& args]
-  `(print (:out (sh ~@args))))
+(defonce pegd (markdown-processor :fenced-code-blocks))
 
 (let [source-dir "/home/tom/dev/tomlynch.io.posts/"
       [resource-dir post-dir template-dir] 
@@ -21,30 +19,43 @@
       post-glob (str post-dir "*.post")
       target-dir "/home/tom/dev/attentive.github.io/"]
 
-  (defconduit page
+  (def nav
+    (html
+      [:div.container-fluid
+       [:ul.nav.navbar.navbar-nav
+        [:li.active [:a {:href "/"} [:h3 "home"]]] 
+        [:li.active [:a {:href "/about"} [:h3 "about"]]] 
+        [:li.active [:a {:href "/archive"} [:h3 "archive"]]] 
+        [:li.active [:a {:href "/categories"} [:h3 "categories"]]] 
+        [:li.active [:a {:href "/tags"} [:h3 "tags"]]]]])) 
+
+  (defconduit post-page
     [post]
     [:head :title] (content (:title post))
-    [:body :div#nav] 
-    (html-content 
-      (html
-        [:div.container-fluid
-         [:ul.nav.navbar.navbar-nav
-          [:li.active [:a {:href "#"} [:h3 "home"]]] 
-          [:li.active [:a {:href "#"} [:h3 "about"]]] 
-          [:li.active [:a {:href "#"} [:h3 "archive"]]] 
-          [:li.active [:a {:href "#"} [:h3 "categories"]]] 
-          [:li.active [:a {:href "#"} [:h3 "tags"]]]]])) 
-    [:body :div#post] (html-content (markdown-to-html md (:body post))))
+    [:body :div#nav] (html-content nav)
+    [:body :div#post] (html-content (markdown-to-html pegd (:body post))))
+
+  (defconduit alpha-index-page
+    [index]
+    [:head :title] (content (:title index))
+    [:body :div#nav] (html-content nav)
+    [:body :div#index :li] (clone-for [item (:items index)]
+                                      [:li] (content item)))
+    
+  (defmacro pr-sh [cmd & args]
+    `(doseq [line# (~cmd ~@args {:seq true})] (println line#)))
 
   (defn publish-post [post dir]
     (let [template (:template post)
           rel-path (:path (ensure-path post))
           path (str dir rel-path)]
-      (mkdirs path)
-      (println "publishing" rel-path)
-      (spit (str path "/post.edn") (pr-str post)) ; pure data now
+      (with-programs [mkdir] (mkdir "-p" path))
+      (println "→" rel-path)
+      ; pure post data
+      (spit (str path "/post.edn") (pr-str post))
+      ; rendered post
       (spit (str path "/index.html") 
-            (apply str (page (str template-dir (:template post)) 
+            (apply str (post-page (str template-dir (:template post)) 
                              post)))))
 
   (defn post-file [post-id]
@@ -62,21 +73,19 @@
   (defn publish-site 
     "Publish an entire site."
     []
-    (println "re-creating site directory" target-dir)
-    (mkdirs target-dir)
-    (doseq [item (list-dir target-dir)]
-      (when (not (hidden? (file item)))
-        (print (:out (sh "rm" "-rvf" (str target-dir item))))))
+    (with-programs [ls cp rm mkdir]
 
-    (println "copying site resources from" resource-dir)
-    (print (:out (sh "cp" "-rv" (str resource-dir "js") target-dir)))
-    (print (:out (sh "cp" "-rv" (str resource-dir "css") target-dir)))
-    (print (:out (sh "cp" "-rv" (str resource-dir "fonts") target-dir)))
-    (print (:out (sh "cp" "-rv" (str resource-dir "app") target-dir)))
-    (print (:out (sh "cp" "-rv" (str resource-dir "img") target-dir)))
+      (pr-sh mkdir "-vp" target-dir)
+      ; delete old
+      (doseq [item (ls target-dir {:seq true})]
+        (pr-sh rm "-rvf" (str target-dir item)))
 
-    (println "publishing posts")
-    (for [post-file (all-post-files)]
-      (publish-post (read-file post-file) target-dir))))
+      ; copy dependencies and resources
+      (doseq [resx ["js" "css" "fonts" "app" "img"]]
+        (pr-sh cp "-rv" (str resource-dir resx) target-dir))
+
+      ; publish
+      (doseq [post-file (all-post-files)]
+        (publish-post (read-file post-file) target-dir)))))
 
 
